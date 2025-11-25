750aa9e9c40108b71cfd3a9e259f7b115b89561d
Airport237 <justingalin2@gmail.com>
2025-11-24 21:05:06 -0500

    MEtrics from SPNV2

--- src/speedplus_utils.py
+++ src/speedplus_utils.py
@@ -51,36 +51,71 @@ def kelvins_orientation_score(R_est, R_gt, tol_deg=0.169):
     R_est, R_gt: (B, 3, 3)
     Returns per-sample orientation score in degrees, with tolerance applied.
     """
-    q_est = rotmat_to_quat(R_est)
-    q_gt  = rotmat_to_quat(R_gt)
+    # q_est = rotmat_to_quat(R_est)
+    # q_gt  = rotmat_to_quat(R_gt)
 
-    # inner product of unit quaternions
-    dot = torch.sum(q_est * q_gt, dim=1)
-    dot = torch.clamp(dot.abs(), -1.0, 1.0)
+    # # inner product of unit quaternions
+    # dot = torch.sum(q_est * q_gt, dim=1)
+    # dot = torch.clamp(dot.abs(), -1.0, 1.0)
 
-    err_rad = 2.0 * torch.acos(dot)
-    err_deg = err_rad * (180.0 / np.pi)
+    # err_rad = 2.0 * torch.acos(dot)
+    # err_deg = err_rad * (180.0 / np.pi)
 
-    # apply tolerance
-    score = err_deg.clone()
-    score[err_deg < tol_deg] = 0.0
-    return score  # (B,)
+    # # apply tolerance
+    # score = err_deg.clone()
+    # score[err_deg < tol_deg] = 0.0
+    # return score  # (B,)
+
+    assert R_est.shape == R_gt.shape and R_est.ndim == 2
+    assert np.abs(np.linalg.det(R_est) - 1) < 1e-6, \
+        f'Determinant of R_pr is {np.linalg.det(R_est)}'
+
+    Rdot = np.dot(R_est, np.transpose(R_gt))
+    trace = (np.trace(Rdot) - 1.0)/2.0
+    trace = np.clip(trace, -1.0, 1.0)
+    return np.rad2deg(np.arccos(trace))
 
 def kelvins_position_score(t_est, t_gt, tol=0.002173):
     """
     t_est, t_gt: (B, 3) in meters
     Returns per-sample position score (dimensionless).
     """
-    diff = torch.norm(t_gt - t_est, dim=1)         # ||r_gt - r_est||
-    denom = torch.norm(t_gt, dim=1).clamp(min=1e-8)  # ||r_gt||
-    err = diff / denom
+    # diff = torch.norm(t_gt - t_est, dim=1)         # ||r_gt - r_est||
+    # denom = torch.norm(t_gt, dim=1).clamp(min=1e-8)  # ||r_gt||
+    # err = diff / denom
 
-    score = err.clone()
-    score[err < tol] = 0.0
-    return score  # (B,)
+    # score = err.clone()
+    # score[err < tol] = 0.0
+    # return score  # (B,)
+    t_est = np.reshape(t_est, (3,))
+    t_gt = np.reshape(t_gt, (3,))
+
+    return np.sqrt(np.sum(np.square(t_gt - t_est)))
 
 
-def kelvins_pose_score(R_est, R_gt, t_est, t_gt):
-    s_orient = kelvins_orientation_score(R_est, R_gt)   # deg
-    s_pos    = kelvins_position_score(t_est, t_gt)      # normalized
-    return s_orient + s_pos, s_orient, s_pos
+# def kelvins_pose_score(R_est, R_gt, t_est, t_gt):
+#     s_orient = kelvins_orientation_score(R_est, R_gt)   # deg
+#     s_pos    = kelvins_position_score(t_est, t_gt)      # normalized
+#     return s_orient + s_pos, s_orient, s_pos
+
+def kelvins_pose_score(t_pr, ori_pr, t_gt, ori_gt, representation='quaternion',
+                    applyThreshold=True, theta_q=0.5, theta_t=0.005):
+    # theta_q: rotation threshold [deg]
+    # theta_t: normalized translation threshold [m/m]
+    err_t = kelvins_orientation_score(t_pr, t_gt)
+    err_q = kelvins_position_score(ori_pr, ori_gt, representation) # [deg]
+
+    t_gt = np.reshape(t_gt, (3,))
+    speed_t = err_t / np.sqrt(np.sum(np.square(t_gt)))
+    speed_q = np.deg2rad(err_q)
+
+    # Check if within threshold
+    if applyThreshold and err_q < theta_q:
+        speed_q = 0.0
+
+    if applyThreshold and speed_t < theta_t:
+        speed_t = 0.0
+
+    speed = speed_t + speed_q
+
+    return speed_t, speed_q, speed
