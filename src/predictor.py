0f42b80603b359541867b6f52cee720fe7856581
Airport237 <justingalin2@gmail.com>
2025-12-08 10:24:46 -0500

    channel

--- src/predictor.py
+++ src/predictor.py
@@ -166,7 +166,7 @@ class I2S(BaseSO3Predictor):
         if self.pred_translation:
             c, h, w = self.encoder.output_shape
             self.translation_head = ResNetTranslationHead(  # can be changed to ResNetTranslationHead
-                in_channels=c, hidden=128)
+                in_channels=c, hidden_channels=128)
         else:
             self.translation_head = None
 
